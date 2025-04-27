cbf576e7c998625204e045724ce50d3ae7d20f9a
Vinay046 <vinayvp.98@gmail.com>
2025-04-27 02:58:24 +0200

    In-file trace_points

--- Makefile
+++ Makefile
@@ -3,8 +3,11 @@ obj-m += fanctrl.o
 
 SRC := $(shell pwd)
 
+# Add the current directory to include path for finding tracepoint headers
+EXTRA_CFLAGS += -I$(SRC)
+
 all:
-	$(MAKE) -C $(KERNEL_SRC) M=$(SRC)
+	$(MAKE) -C $(KERNEL_SRC) M=$(SRC) modules
 
 modules_install:
 	$(MAKE) -C $(KERNEL_SRC) M=$(SRC) modules_install
--- fanctrl.c
+++ fanctrl.c
@@ -3,17 +3,52 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/tracepoint.h>
-#include <trace/events/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Your Name");
 MODULE_DESCRIPTION("Fan Control module with custom tracing");
 MODULE_VERSION("0.1");
 
-/* Define tracepoint */
-#define CREATE_TRACE_POINTS
-#include "fanctrl_tp.h"
+/* Define tracepoint system */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM fanctrl
 
+/* 
+ * For in-file tracepoints, we need to specify the include path
+ * to be the current directory
+ */
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE fanctrl
+
+/* 
+ * Define the tracepoint before including define_trace.h
+ * First we need the tracepoint definition
+ */
+#if !defined(_FANCTRL_TP_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _FANCTRL_TP_H
+
+TRACE_EVENT(fanctrl_event,
+    TP_PROTO(const char *message),
+    TP_ARGS(message),
+    TP_STRUCT__entry(
+        __string(message, message)
+    ),
+    TP_fast_assign(
+        __assign_str(message, message);
+    ),
+    TP_printk("FanCtrl: %s", __get_str(message))
+);
+
+#endif /* _FANCTRL_TP_H */
+
+/* 
+ * Now create the tracepoint function
+ * This must be done after the tracepoint definition but before the module code
+ */
+#define CREATE_TRACE_POINTS
+#include <trace/define_trace.h>
+
+/* Module code starts here */
 static int __init fanctrl_init(void)
 {
     printk(KERN_INFO "FanCtrl Module: Initializing\n");
