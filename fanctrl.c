d9c0fe4c8be332039d0bb731af33fe949640163b
Vinay046 <vinayvp.98@gmail.com>
2025-04-27 12:30:45 +0200

    General clean up and removing trace_points

--- fanctrl.c
+++ fanctrl.c
@@ -1,57 +1,23 @@
-#include <linux/init.h>
+// fanctrl.c
+
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Vinay");
-MODULE_DESCRIPTION("Fan Control module with custom tracing");
-MODULE_VERSION("0.1");
-
-/* Tracepoint setup */
-#undef TRACE_SYSTEM
-#define TRACE_SYSTEM fanctrl
-
-#define TRACE_INCLUDE_PATH .
-#define TRACE_INCLUDE_FILE fanctrl
-
-#if !defined(_FANCTRL_TP_H) || defined(TRACE_HEADER_MULTI_READ)
-#define _FANCTRL_TP_H
-
-#include <linux/tracepoint.h>
-
-TRACE_EVENT(fanctrl_event,
-    TP_PROTO(const char *message),
-    TP_ARGS(message),
-    TP_STRUCT__entry(
-        __string(message, message)
-    ),
-    TP_fast_assign(
-        __assign_str(message, message);
-    ),
-    TP_printk("FanCtrl: %s", __get_str(message))
-);
-
-#endif /* _FANCTRL_TP_H */
-
-#define CREATE_TRACE_POINTS
-#include <trace/define_trace.h>
-
-/* Actual module code */
 static int __init fanctrl_init(void)
 {
-    pr_info("FanCtrl Module: Initializing\n");
-    
-    trace_fanctrl_event("Module initialized");
-    trace_fanctrl_event("Hello from fan controller!");
-    
+    printk(KERN_INFO "Hello, Loading fanctrl kernel module\n");
     return 0;
 }
 
 static void __exit fanctrl_exit(void)
 {
-    pr_info("FanCtrl Module: Exiting\n");
-    trace_fanctrl_event("Module exiting");
+    printk(KERN_INFO "Goodbye, Unloading fanctrl kernel module\n");
 }
 
 module_init(fanctrl_init);
 module_exit(fanctrl_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Vinay");
+MODULE_DESCRIPTION("Kernel Module to control fan speed based on CPU temps");
